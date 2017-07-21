01bc2fb5d1711ea66850bf89d5ac23d1af2865c5
michaelzoidl <github@michaelzoidl.com>
2017-07-21 15:30:58 +0200

    add inquirer

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
+const inquirer = require('inquirer')
 
-console.log('hello')
+const steps = [{
+  type: 'input',
+  name: 'userName',
+  message: 'Whats your name?'
+}]
+inquirer
+  .prompt(steps).then(output => {
+  console.log(
+    'hello',
+    output.userName
+  );
+}).catch(err => console.log(err))
